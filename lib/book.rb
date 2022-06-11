fdb2b815eee4d59516d2da4c750c4a0fb0555fd3
Njoki <kinuthiash@gmail.com>
2022-06-11 23:14:04 +0300

    commit tree 7db8df7f6393af52d5cc6d197b622a31106bc2cf

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
 
+  attr_reader :title
+  attr_accessor :author, :genre, :page_count
+
+  def initialize title
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+
+book1 = Book.new("Atomic habits")
+book1.author= "James clear"
+book1.genre= "self improvement"
+book1.page_count= 272
+puts book1.turn_page
 
